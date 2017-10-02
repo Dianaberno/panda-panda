ef5b6853d7d14be9e92f9c9c80543f639cf11c4f
paola <bernodiana@gmail.com>
2017-10-02 09:06:44 -0500

    haciendo con jquery

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $("#referenciaa").click(function() {
+        $("#elemento1").hide();
+    });
+    $("#referenciab").click(function() {
+        $("#elemento2").hide();
+    });
+    $("#referenciac").click(function() {
+        $("#elemento3").hide();
+    });
+    $("#referenciad").click(function() {
+        $("#elemento4").hide();
+    });
+    $("#mostrar").click(function() {
+        $("img").show();
+    });
+});
